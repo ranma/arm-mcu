d58a6571e62ff7423020871d557b48e01f23627f
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2015-05-05 11:10:12 +0000

    Pass linker flags to gprbuild
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@8855 06ee2f96-5407-11de-8bab-f3d7589a4122

--- gnat/ada.mk
+++ gnat/ada.mk
@@ -41,6 +41,7 @@ GNATRTS			= $(ADA_SRC)/$(MCUFAMILY)/ravenscar-sfp-stm32f4
 GPRBUILD		= env PATH=$(GNATBIN)':'$(PATH) ADA_SRC=$(ADA_SRC) ADA_OBJ=$(ADA_OBJ) MCUFAMILY=$(MCUFAMILY) BOARDNAME=$(BOARDNAME) gprbuild
 GPRBUILDFLAGS		= -p --RTS=$(GNATRTS) --target=$(GNATARCH)
 GPRBUILDCFLAGS		= -cargs:Ada $(ADA_VERSION) $(ADA_CFLAGS)
+GPRBUILDLDFLAGS		= -largs -Wl,-Map=test_console.map,--cref
 
 # Override the following macros previously defined in ARM.mk
 
@@ -53,7 +54,7 @@ STLINKDEBUG		= $(ADA_SRC)/ada.gdb
 
 %.elf: %.gpr
 	$(MAKE) -C $(GNATRTS) ADA_SRC=$(ADA_SRC) MCUFAMILY=$(MCUFAMILY) BOARDNAME=$(BOARDNAME)
-	$(GPRBUILD) $< $(GPRBUILDFLAGS) $(GPRBUILDCFLAGS)
+	$(GPRBUILD) $< $(GPRBUILDFLAGS) $(GPRBUILDCFLAGS) $(GPRBUILDLDFLAGS)
 
 # Default make target
 
