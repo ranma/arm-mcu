69326e281f86fbd7317453f62dead69d24aecefc
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2015-05-20 19:09:24 +0000

    Added adc.o to library objects
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@8954 06ee2f96-5407-11de-8bab-f3d7589a4122

--- stm32f4/stm32f4.mk
+++ stm32f4/stm32f4.mk
@@ -50,7 +50,7 @@ include $(MCUDIR)/CMSIS/cmsis.mk
 
 # Build processor dependent support library
 
-LIBOBJS		= $(MCU).o cpu.o gpiopins.o leds.o serial.o $(EXTRALIBOBJS)
+LIBOBJS		= $(MCU).o adc.o cpu.o gpiopins.o leds.o serial.o $(EXTRALIBOBJS)
 
 lib$(MCU).a: $(LIBOBJS)
 	$(AR) crs lib$(MCU).a $(LIBOBJS)
