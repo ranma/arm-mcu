5ea851d60d179d633064fe88e907a693f7c9d4b6
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2015-05-20 12:32:34 +0000

    Copied leds.c from stm32f4/
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@8945 06ee2f96-5407-11de-8bab-f3d7589a4122

--- stm32f1/leds.c
+++ stm32f1/leds.c
@@ -30,23 +30,36 @@ static const char revision[] = "$Id$";
 
 void LEDS_initialize(void)
 {
-#ifdef OLIMEX_STM32_P103
-  gpiopin_configure(GPIOPIN44, GPIOPIN_OUTPUT);
+#ifdef LED0_PIN
+  gpiopin_configure(LED0_PIN, GPIOPIN_OUTPUT);
 #endif
 
-#ifdef OLIMEX_STM32_P107
-  gpiopin_configure(GPIOPIN38, GPIOPIN_OUTPUT);
-  gpiopin_configure(GPIOPIN39, GPIOPIN_OUTPUT);
+#ifdef LED1_PIN
+  gpiopin_configure(LED1_PIN, GPIOPIN_OUTPUT);
 #endif
 
-#ifdef STM32_VALUE_LINE_DISCOVERY
-  gpiopin_configure(GPIOPIN40, GPIOPIN_OUTPUT);
-  gpiopin_configure(GPIOPIN41, GPIOPIN_OUTPUT);
+#ifdef LED2_PIN
+  gpiopin_configure(LED2_PIN, GPIOPIN_OUTPUT);
 #endif
 
-#ifdef W5200E01_M3
-  gpiopin_configure(GPIOPIN0, GPIOPIN_OUTPUT);
-  gpiopin_configure(GPIOPIN1, GPIOPIN_OUTPUT);
+#ifdef LED3_PIN
+  gpiopin_configure(LED3_PIN, GPIOPIN_OUTPUT);
+#endif
+
+#ifdef LED4_PIN
+  gpiopin_configure(LED4_PIN, GPIOPIN_OUTPUT);
+#endif
+
+#ifdef LED5_PIN
+  gpiopin_configure(LED5_PIN, GPIOPIN_OUTPUT);
+#endif
+
+#ifdef LED6_PIN
+  gpiopin_configure(LED6_PIN, GPIOPIN_OUTPUT);
+#endif
+
+#ifdef LED7_PIN
+  gpiopin_configure(LED7_PIN, GPIOPIN_OUTPUT);
 #endif
 
   LEDS_set(0);						// Turn off all LEDs at startup
@@ -59,23 +72,68 @@ unsigned long int LEDS_get(void)
 {
   unsigned long int result = 0;
 
-#ifdef OLIMEX_STM32_P103
-  result += !GPIOPIN44IN;
+#ifdef LED0_INPUT
+#ifdef LED0_ACTIVELOW
+  result += !LED0_INPUT;
+#else
+  result += LED0_INPUT;
+#endif
 #endif
 
-#ifdef OLIMEX_STM32_P107
-  result += GPIOPIN38IN;
-  result += GPIOPIN39IN << 1;
+#ifdef LED1_INPUT
+#ifdef LED1_ACTIVELOW
+  result += !LED1_INPUT << 1;
+#else
+  result += LED1_INPUT << 1;
+#endif
 #endif
 
-#ifdef STM32_VALUE_LINE_DISCOVERY
-  result += GPIOPIN40IN;
-  result += GPIOPIN41IN << 1;
+#ifdef LED2_INPUT
+#ifdef LED2_ACTIVELOW
+  result += !LED2_INPUT << 2;
+#else
+  result += LED2_INPUT << 2;
+#endif
 #endif
 
-#ifdef W5200E01_M3
-  result += !GPIOPIN0IN;
-  result += !GPIOPIN1IN << 1;
+#ifdef LED3_INPUT
+#ifdef LED3_ACTIVELOW
+  result += !LED3_INPUT << 3;
+#else
+  result += LED3_INPUT << 3;
+#endif
+#endif
+
+#ifdef LED4_INPUT
+#ifdef LED4_ACTIVELOW
+  result += !LED4_INPUT << 4;
+#else
+  result += LED4_INPUT << 4;
+#endif
+#endif
+
+#ifdef LED5_INPUT
+#ifdef LED5_ACTIVELOW
+  result += !LED5_INPUT << 5;
+#else
+  result += LED5_INPUT << 5;
+#endif
+#endif
+
+#ifdef LED6_INPUT
+#ifdef LED6_ACTIVELOW
+  result += !LED6_INPUT << 6;
+#else
+  result += LED6_INPUT << 6;
+#endif
+#endif
+
+#ifdef LED7_INPUT
+#ifdef LED7_ACTIVELOW
+  result += !LED7_INPUT << 7;
+#else
+  result += LED7_INPUT << 7;
+#endif
 #endif
 
   return result;
@@ -86,22 +144,67 @@ unsigned long int LEDS_get(void)
 
 void LEDS_set(unsigned long int mask)
 {
-#ifdef OLIMEX_STM32_P103
-  GPIOPIN44OUT = ~mask;
+#ifdef LED0_OUTPUT
+#ifdef LED0_ACTIVELOW
+  LED0_OUTPUT = ~mask;
+#else
+  LED0_OUTPUT = mask;
+#endif
 #endif
 
-#ifdef OLIMEX_STM32_P107
-  GPIOPIN38OUT = mask;
-  GPIOPIN39OUT = mask >> 1;
+#ifdef LED1_OUTPUT
+#ifdef LED1_ACTIVELOW
+  LED1_OUTPUT = ~mask >> 1;
+#else
+  LED1_OUTPUT = mask >> 1;
+#endif
 #endif
 
-#ifdef STM32_VALUE_LINE_DISCOVERY
-  GPIOPIN40OUT = mask;
-  GPIOPIN41OUT = mask >> 1;
+#ifdef LED2_OUTPUT
+#ifdef LED2_ACTIVELOW
+  LED2_OUTPUT = ~mask >> 2;
+#else
+  LED2_OUTPUT = mask >> 2;
+#endif
 #endif
 
-#ifdef W5200E01_M3
-  GPIOPIN0OUT = ~mask;
-  GPIOPIN1OUT = ~mask >> 1;
+#ifdef LED3_OUTPUT
+#ifdef LED3_ACTIVELOW
+  LED3_OUTPUT = ~mask >> 3;
+#else
+  LED3_OUTPUT = mask >> 3;
+#endif
+#endif
+
+#ifdef LED4_OUTPUT
+#ifdef LED4_ACTIVELOW
+  LED4_OUTPUT = ~mask >> 4;
+#else
+  LED4_OUTPUT = mask >> 4;
+#endif
+#endif
+
+#ifdef LED5_OUTPUT
+#ifdef LED5_ACTIVELOW
+  LED5_OUTPUT = ~mask >> 5;
+#else
+  LED5_OUTPUT = mask >> 5;
+#endif
+#endif
+
+#ifdef LED6_OUTPUT
+#ifdef LED6_ACTIVELOW
+  LED6_OUTPUT = ~mask >> 6;
+#else
+  LED6_OUTPUT = mask >> 6;
+#endif
+#endif
+
+#ifdef LED7_OUTPUT
+#ifdef LED7_ACTIVELOW
+  LED7_OUTPUT = ~mask >> 7;
+#else
+  LED7_OUTPUT = mask >> 7;
+#endif
 #endif
 }
