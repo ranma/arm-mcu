cc49811f29e5c8ffebe083e4562ee28dbd15dd64
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2015-05-20 11:53:25 +0000

    Allow for active low LED's
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@8938 06ee2f96-5407-11de-8bab-f3d7589a4122

--- stm32f4/leds.c
+++ stm32f4/leds.c
@@ -73,35 +73,67 @@ unsigned long int LEDS_get(void)
   unsigned long int result = 0;
 
 #ifdef LED0_INPUT
+#ifdef LED0_ACTIVELOW
+  result += !LED0_INPUT;
+#else
   result += LED0_INPUT;
+#endif
 #endif
 
 #ifdef LED1_INPUT
+#ifdef LED1_ACTIVELOW
+  result += !LED1_INPUT << 1;
+#else
   result += LED1_INPUT << 1;
+#endif
 #endif
 
 #ifdef LED2_INPUT
+#ifdef LED2_ACTIVELOW
+  result += !LED2_INPUT << 2;
+#else
   result += LED2_INPUT << 2;
+#endif
 #endif
 
 #ifdef LED3_INPUT
+#ifdef LED3_ACTIVELOW
+  result += !LED3_INPUT << 3;
+#else
   result += LED3_INPUT << 3;
+#endif
 #endif
 
 #ifdef LED4_INPUT
+#ifdef LED4_ACTIVELOW
+  result += !LED4_INPUT << 4;
+#else
   result += LED4_INPUT << 4;
+#endif
 #endif
 
 #ifdef LED5_INPUT
+#ifdef LED5_ACTIVELOW
+  result += !LED5_INPUT << 5;
+#else
   result += LED5_INPUT << 5;
+#endif
 #endif
 
 #ifdef LED6_INPUT
+#ifdef LED6_ACTIVELOW
+  result += !LED6_INPUT << 6;
+#else
   result += LED6_INPUT << 6;
+#endif
 #endif
 
 #ifdef LED7_INPUT
+#ifdef LED7_ACTIVELOW
+  result += !LED7_INPUT << 7;
+#else
   result += LED7_INPUT << 7;
+#endif
 #endif
 
   return result;
@@ -113,34 +145,66 @@ unsigned long int LEDS_get(void)
 void LEDS_set(unsigned long int mask)
 {
 #ifdef LED0_OUTPUT
+#ifdef LED0_ACTIVELOW
+  LED0_OUTPUT = ~mask;
+#else
   LED0_OUTPUT = mask;
+#endif
 #endif
 
 #ifdef LED1_OUTPUT
+#ifdef LED1_ACTIVELOW
+  LED1_OUTPUT = ~mask >> 1;
+#else
   LED1_OUTPUT = mask >> 1;
+#endif
 #endif
 
 #ifdef LED2_OUTPUT
+#ifdef LED2_ACTIVELOW
+  LED2_OUTPUT = ~mask >> 2;
+#else
   LED2_OUTPUT = mask >> 2;
+#endif
 #endif
 
 #ifdef LED3_OUTPUT
+#ifdef LED3_ACTIVELOW
+  LED3_OUTPUT = ~mask >> 3;
+#else
   LED3_OUTPUT = mask >> 3;
+#endif
 #endif
 
 #ifdef LED4_OUTPUT
+#ifdef LED4_ACTIVELOW
+  LED4_OUTPUT = ~mask >> 4;
+#else
   LED4_OUTPUT = mask >> 4;
+#endif
 #endif
 
 #ifdef LED5_OUTPUT
+#ifdef LED5_ACTIVELOW
+  LED5_OUTPUT = ~mask >> 5;
+#else
   LED5_OUTPUT = mask >> 5;
+#endif
 #endif
 
 #ifdef LED6_OUTPUT
+#ifdef LED6_ACTIVELOW
+  LED6_OUTPUT = ~mask >> 6;
+#else
   LED6_OUTPUT = mask >> 6;
+#endif
 #endif
 
 #ifdef LED7_OUTPUT
+#ifdef LED7_ACTIVELOW
+  LED7_OUTPUT = ~mask >> 7;
+#else
   LED7_OUTPUT = mask >> 7;
+#endif
 #endif
 }
